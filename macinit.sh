ed50c194beaba54d7400374c4a348b4c191d4701
zhuiyu <zhuiyu.xn@alibaba-inc.com>
2017-08-29 22:54:41 +0800

    init

---
+++ macinit.sh
@@ -0,0 +1,56 @@
+
+## install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+## brew install software
+brew install wget
+brew install trash
+brew install balance
+brew install enca
+brew install lrzsz
+
+## brew install programming tools
+brew install highlight
+brew install jadx
+brew install groovy
+brew install lua
+brew install node
+brew install redis
+brew install mariadb
+
+## install brew cask
+brew tap caskroom/cask
+
+## brew cask install software
+brew cask install qlstephen
+brew cask install qlmarkdown
+brew cask install qlimagesize
+brew cask install qlvideo
+brew cask install qlcolorcode
+brew cask install qlprettypatch
+brew cask install quicklook-csv
+brew cask install quicklook-json
+brew cask install quicklookapk
+brew cask install webpquicklook
+
+brew cask install the-unarchiver
+brew cask install google-chrome
+brew cask install 5kplayer
+brew cask install tickeys
+brew cask install go2shell
+brew cask install qiyimedia
+brew cask install youdaodict
+
+## brew cask install programming tools
+brew cask install dingtalk
+brew cask install iterm2
+brew cask install macdown
+brew cask install sublime-text
+brew cask install visual-studio-code
+brew cask install dash3
+brew cask install medis
+brew cask install mysqlworkbench
+brew cask install cocoarestclient
+brew cask install sequel-pro
+brew cask install cakebrew
+
